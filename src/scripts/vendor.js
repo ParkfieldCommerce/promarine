069243ba967a075a0a8d30035b058fd65ea8222e
Leoncio Perez <lperez@petcreds.com>
2017-12-07 14:35:20 -0500

    init commit

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -28,6 +28,11 @@
  */
 // =require vendor/Cartfox.js
 
+/*!
+ * slick.js
+ */
+// =require vendor/slick.js
+
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
 //
